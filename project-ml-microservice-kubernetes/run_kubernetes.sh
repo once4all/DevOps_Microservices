e8166acae0df1029fadf70f7fc550a4164f8b6ae
Tu Nguyen <>
2023-01-03 17:57:09 +0700

    update code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=kutinka123/predict-app:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f deployment.yaml
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl apply -f service.yaml
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=kutinka123/predict-app:latest
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag predict-app $dockerpath
+docker login --username kutinka123 --password "Tunx06)^!((!"
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
